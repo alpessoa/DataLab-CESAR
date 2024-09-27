40a1e6cc437ff8a4b5a784cb789953acded6b2e9
Apollo <andre.pessoa92@hotmail.com>
2024-09-26 22:17:19 -0300

    Adiciona o arquivo Dia01.py

---
+++ Dia01.py
@@ -0,0 +1,9 @@
+#Criação do sistema de gerenciamento de estoque, onde o usuário poderá adicionar, remover, visualizar produtos e calcular o valor total do estoque.
+
+estoque = ["Caneta", "Lápis", "Apontador", "Borracha", "Tesoura", "Papel", "Régua", "Marcador", "Caderno", "Mochila"]
+
+estoque.append("Quadro-negro")
+estoque.append("Piloto")
+print(estoque)
+estoque.pop(0)
+print(estoque)
